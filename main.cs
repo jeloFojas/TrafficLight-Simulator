0b8830b52bdce48e1b0810167433cc266130ebde
jeloFojas <jelo.fojas@deped.gov.ph>
2025-01-29 02:01:23 +0000

    Initial Commit

--- main.cs
+++ main.cs
@@ -2,6 +2,25 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("=====  TRAFFIC LIGHT SIMULATOR     =====");
+    Console.ReadLine();
+
+    Console.Write("Enter the current color of the traffic light: ");
+    string userInput = Console.ReadLine();
+
+    if (userInput.Equals("red",StringComparison.CurrentCultureIgnoreCase)){
+      Console.WriteLine($"Traffic Light Color: {userInput}");
+      Console.WriteLine("Vehicle Status: Stop!");
+    } 
+    else if (userInput.Equals("red",StringComparison.CurrentCultureIgnoreCase)){
+      Console.WriteLine($"Traffic Light Color: {userInput}");
+      Console.WriteLine("Vehicle Status: Go!");
+    } 
+    else if (userInput.Equals("yellow",StringComparison.CurrentCultureIgnoreCase)){
+      Console.WriteLine($"Traffic Light Color: {userInput}");
+      Console.WriteLine("Vehicle Status: Slow Down!");
+    } else {
+      Console.WriteLine("Invalid Traffic Light Color");
+    }
   }
 }
